0b5b13f90563ae1cfff1259a6fbc39956d00234f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-31 14:56:40 -0500

    bug siteconfig and picture

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: ` https://juanblog.herokuapp.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://juanblog.herokuapp.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
